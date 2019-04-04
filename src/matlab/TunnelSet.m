a842a613de77c85f328dc84e7a34a84757fcff74
Tien-Yiao Hsu <tienyiao@cheyenne4.ib0.cheyenne.ucar.edu>
2019-04-03 20:11:55 -0600

    Done most of the function. Need to add binary related functions and test them

---
+++ src/matlab/TunnelSet.m
@@ -0,0 +1,49 @@
+classdef TunnelSet < handle
+    properties
+        tnls
+    end
+    methods
+        function self = TunnelSet(path, recv_txt, send_txt, recv_bin, send_bin)
+            self.tnls = containers.Map;
+            self.tnls('recv_txt') = Tunnel(path, recv_txt);
+            self.tnls('send_txt') = Tunnel(path, send_txt);
+            self.tnls('recv_bin') = Tunnel(path, recv_bin);
+            self.tnls('send_bin') = Tunnel(path, send_bin);
+        end
+
+        function reverseRole(self)
+            [self.tnls('recv_txt'), self.tnls('send_txt')] = swap(self.tnls('recv_txt'), self.tnls('send_txt'));
+            [self.tnls('recv_bin'), self.tnls('send_bin')] = swap(self.tnls('recv_bin'), self.tnls('send_bin'));
+        end
+
+        function fn = getTunnelFilename(self, key)
+            tnl = self.tnls(key);
+            fn = tnl.getTunnelFilename();
+        end
+
+        function msg = recvText(self)
+            fd = fopen(self.getTunnelFilename('recv_txt'), 'r');
+            msg = self.parse(fscanf(fd, '%s'));
+            fclose(fd);
+        end 
+
+        function sendText(self, msg)
+            fd = fopen(self.getTunnelFilename('send_txt'), 'w');
+            fwrite(fd, msg);
+            fclose(fd);
+        end
+
+        function obj = parse(self, msg)
+            obj = containers.Map
+            pieces = split(msg, ';')
+            for i = 1 : length(pieces)
+                kvpair = split(pieces(i), ':')
+                if length(kvpair) == 2
+                    obj(char(kvpair(1))) = char(kvpair(2))
+                end
+            end
+        end
+    end
+end
+
+
---
+++ src/matlab/defaultTunnelSet.m
@@ -0,0 +1,3 @@
+function o = defaultTunnelSet(path)
+    o = TunnelSet(path, 'X2Y_txt', 'Y2X_txt', 'X2Y_bin', 'Y2X_bin')
+end
